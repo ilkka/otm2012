c18744e9f9b54e00add4209c101f48a9bf0cd9dc
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2012-02-27 19:54:59 +0200

    Checkout images and js too when publishing

--- publish.sh
+++ publish.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+FILES="index.html img myjs"
 which git >/dev/null 2>&1 || (echo "Git not in PATH" >2 && exit 1)
 git remote|grep -q impressjs || git remote add impressjs https://github.com/bartaz/impress.js.git
 git checkout gh-pages
-git checkout master -- index.html prototypescrum.html
+git checkout master -- $FILES
 git fetch impressjs
 git checkout impressjs/master -- js/impress.js
-git add index.html prototypescrum.html js/impress.js
+git add $FILES js/impress.js
 git commit -v
 git push
 git checkout master
