e6aac0b624e1a22a4f460a623382424f74d2bb22
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2012-02-27 19:48:49 +0200

    Include prototype scrum page in checkout

--- publish.sh
+++ publish.sh
@@ -2,10 +2,10 @@
 which git >/dev/null 2>&1 || (echo "Git not in PATH" >2 && exit 1)
 git remote|grep -q impressjs || git remote add impressjs https://github.com/bartaz/impress.js.git
 git checkout gh-pages
-git checkout master -- index.html
+git checkout master -- index.html prototypescrum.html
 git fetch impressjs
 git checkout impressjs/master -- js/impress.js
-git add index.html js/impress.js
+git add index.html prototypescrum.html js/impress.js
 git commit -v
 git push
 git checkout master
