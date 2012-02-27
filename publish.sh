21e5e9a2d84d1628241bfdf99fcfdeaa278ff00d
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2012-02-27 20:02:36 +0200

    Include css in publish

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-FILES="index.html lectures img myjs"
+FILES="index.html lectures img myjs css"
 which git >/dev/null 2>&1 || (echo "Git not in PATH" >2 && exit 1)
 git remote|grep -q impressjs || git remote add impressjs https://github.com/bartaz/impress.js.git
 git checkout gh-pages
